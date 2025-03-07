3bd873df6d3eb7c761adb57b9c8f46d923021988
Stephen <stephen.t.j.leong@gmail.com>
2025-03-07 18:50:11 +0000

    Initial backend set up

---
+++ backend/database.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise()
