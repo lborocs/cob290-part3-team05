fae6ad2a8c46fdcfe3beccde8073eb1e39c465d6
Stephen <stephen.t.j.leong@gmail.com>
2025-05-07 20:34:19 +0100

    Add disable input for private chat

--- frontend/src/components/chats/modals/CreateChatModal.jsx
+++ frontend/src/components/chats/modals/CreateChatModal.jsx
@@ -87,6 +87,7 @@ const CreateChatModal = ({ onCancel, onCreateChat, currentUserID }) => {
               className="w-full px-3 py-2 rounded border border-gray-300 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter chat title"
               required
+              disabled={chatType === "Private"}
             />
           </div>
 
